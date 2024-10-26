c7d6462a58e535c700477cc37d5df2739e96f44a
Miguel <dasilvamigueleduardo@gmail.com>
2024-10-26 19:49:58 -0300

    primeiro commit

---
+++ Dominio/DTOs/loginDTO.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MinimalAPI.DTOs;
+
+public class LoginDTO
+{
+    public String Email { get; set; } = default!;
+    public String Senha { get; set; } = default!;
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapPost("/login", (MinimalAPI.DTOs.LoginDTO LoginDTO) =>
+{
+    if (LoginDTO.Email == "adm@teste.com" && LoginDTO.Senha == "123456")
+        return Results.Ok("Login com sucesso");
+    else
+        return Results.Unauthorized();
+});
+
+app.Run();
